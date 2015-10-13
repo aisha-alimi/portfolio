4b319b5e9a72cd1e50625378494541b505a1058f
codecademy <ccuser@codecademy.com>
2015-10-13 09:35:08 -0400

    Commit 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#welcome'
-
+  get 'portfolio' => 'pages#portfolio'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
